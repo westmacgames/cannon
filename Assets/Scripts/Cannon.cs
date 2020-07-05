6c1cb50b1ae41b80b78b4d72ae7526cabdb0d303
Weston McNamara <westonfmcnamara@gmail.com>
2020-07-05 11:58:44 -0400

    Added EZCameraShake implementation

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class Cannon : MonoBehaviour
 {
-    [Range(5f, 50f)]
+    [Header("Cannon Settings")]
+    [Range(25f, 45f)]
     public float power;
 
     [Range(1f, 3f)]
     public float sensitivity;
 
     [SerializeField]
+    [Tooltip("The point where the ball is shot from. The blue/forward axis should point out from the cannon.")]
     private Transform shootPoint = null;
 
     [SerializeField]
+    [Tooltip("Cannonball prefab to be shot out from shootPoint")]
     private GameObject cannonBall = null;
 
+    [Header("Camera Shake")]
+    [Space]
+
+    [Range(5f, 15f)] public float magn;
+    [Range(5f, 15f)] public float rough;
+    [Range(0.0f, 1.5f)] public float fadeIn;
+    [Range(0.5f, 2f)] public float fadeOut;
+  
     private CannonInput input;
     private void Awake()
     {
@@ -31,6 +43,7 @@ public class Cannon : MonoBehaviour
 
     void Shoot()
     {
+        CameraShaker.Instance.ShakeOnce(magn, rough, fadeIn, fadeOut);
         GameObject newCannonBall = Instantiate(cannonBall, shootPoint.position, Quaternion.identity);
         newCannonBall.GetComponent<Rigidbody>().AddForce(shootPoint.transform.forward * 100 * power);
     }
